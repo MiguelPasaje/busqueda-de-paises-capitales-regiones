29b731857568932794862a2427a51c892ec0dfc0
MiguelPasaje <cartel-dy@hotmail.com>
2024-03-02 21:22:31 -0500

    mejorando el servicio - codigo repetitivo

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, map, of } from 'rxjs';
+import { Observable, catchError, delay, map, of } from 'rxjs';
 import { Country } from '../interfaces/country';
 import { Pais } from '../interfaces/pais';
 
@@ -10,26 +10,36 @@ export class CountriesService {
 
   constructor(private http: HttpClient) {}
 
+  private getCountriesRequest(url: string): Observable<Country[]> {
+    return this.http.get<Country[]>(url).pipe(
+      catchError((error) => of([])),
+      delay(2000)
+    );
+  }
+
   searchCountryByAlphaCode(code: string): Observable<Country | null> {
     const url = `${this.apiUrl}/alpha/${code}`;
-    return this.http.get<Country[]>(url)
-    .pipe(
-      map( countries => countries.length ? countries[0] : null), 
-      catchError((error) => of(null)));
+    return this.http.get<Country[]>(url).pipe(
+      map((countries) => (countries.length ? countries[0] : null)),
+      catchError((error) => of(null))
+    );
   }
 
   searchCapital(term: string): Observable<Country[]> {
     const url = `${this.apiUrl}/capital/${term}`;
-    return this.http.get<Country[]>(url).pipe(catchError((error) => of([])));
+    return this.getCountriesRequest(url);
+    //return this.http.get<Country[]>(url).pipe(catchError((error) => of([])));
   }
 
   searchCountry(term: string): Observable<Country[]> {
     const url = `${this.apiUrl}/name/${term}`;
-    return this.http.get<Country[]>(url).pipe(catchError((error) => of([])));
+    return this.getCountriesRequest(url);
+    //return this.http.get<Country[]>(url).pipe(catchError((error) => of([])));
   }
 
   searchRegion(term: string): Observable<Country[]> {
     const url = `${this.apiUrl}/region/${term}`;
-    return this.http.get<Country[]>(url).pipe(catchError((error) => of([])));
+    return this.getCountriesRequest(url);
+    //return this.http.get<Country[]>(url).pipe(catchError((error) => of([])));
   }
 }
