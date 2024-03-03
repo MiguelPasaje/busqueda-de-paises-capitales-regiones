a94b9bdad1b20eea95c6a02e7f52cbed6ce2361e
MiguelPasaje <cartel-dy@hotmail.com>
2024-03-02 23:10:52 -0500

    cambio en by-region-page por botones, ya que es mas limitada la busqueda

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,22 +2,31 @@ import { Component } from '@angular/core';
 import { CountriesService } from '../../services/countries.service';
 import { Country } from '../../interfaces/country';
 
+type Region = 'Africa' | 'Americas' | 'Asia' | 'Europe' | 'Oceania';
+
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html',
   styles: [],
 })
 export class ByRegionPageComponent {
-
-  public regiones: Country[] = []
+  public regiones: Country[] = [];
+  public regions: Region[] = [
+    'Africa',
+    'Americas',
+    'Asia',
+    'Europe',
+    'Oceania',
+  ];
+  public selectedRegion?: Region;
 
   constructor(private countriesService: CountriesService) {}
 
-  searchByRegion(term: string) {
-    this.countriesService.searchRegion(term).subscribe((data)=>{
+  searchByRegion(term: Region) {
+    this.selectedRegion = term;
+    this.countriesService.searchRegion(term).subscribe((data) => {
       console.log(data);
-      this.regiones = data
-
-    })
+      this.regiones = data;
+    });
   }
 }
